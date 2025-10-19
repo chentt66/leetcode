4869928f398cd9a6c7de5b453ae51961a5900310
chentt66 <tc3216@columbia.edu>
2025-10-18 19:08:32 -0700

    Detect cycle in a linked list(two methods)

---
+++ linked_list_cycle.141.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+// Floyd's Cycle Detection Algorithm: O(n) time, O(1) space
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (!head) return false;
+        ListNode* fast = head->next;
+        ListNode* slow = head;
+        while (fast != slow) {
+        if (fast == nullptr || fast->next == nullptr) return false;
+        // e.g. ... → 1 → 2 → 3 → null.  no cycle
+        // !fast: suppose fast at 2. fast->next->next is null.
+        // !fast->next: suppose fast at 3. fast->next is null. fast->next->next will crash.
+        fast = fast->next->next;
+        slow = slow->next;
+        }
+        return true;
+    }
+};
+
+// Track occurences: O(n) time, O(n) space
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         std::unordered_set<ListNode*> seenNodes;
+//         ListNode* currentNode = head;
+//         while (currentNode != nullptr) {
+//             if (seenNodes.find(currentNode) != seenNodes.end()) {
+//                 return true;
+//             }
+//             seenNodes.insert(currentNode);
+//             currentNode = currentNode->next;
+//         }
+//         return false;
+//     }
+// };
