ac582e87b0a10ada35af0a40cefc0f1a6a70ecec
chentt66 <tc3216@columbia.edu>
2025-10-19 22:11:12 -0700

    Traverse a linked list and handle edge cases

---
+++ remove_nth_node_from_end_of_list.19.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// e.g. length = 5, n = 2
+// --> delete the 4th node, whose index is length - n = 3.
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* current = head;
+        int length = 0;
+        // Traverse and get the length
+        while (current != nullptr) {
+            ++length;
+            current = current->next;
+        }
+        // edge case: remove the head
+        if (length == n) {
+            ListNode* newHead = head->next;
+            delete head;
+            return newHead;
+        }
+        // general case:
+        current = head;
+        for (int i = 0; i < length - n - 1; i++) {
+            current = current->next;
+        }
+        current->next = current->next->next;
+        return head;
+    }
+};
+// To remove a node, we need access to the node BEFORE it.
+// if n = length, we need to delete the head node, and there is no node before it in the linked list.
+// so we need to create a dummy node before the head to handle this case.
+// ListNode* dummy = new ListNode(0);
+// dummy->next = head;
+// current = dummy;
+// for (int i = 0; i < length - n; ++i) {
+//     current = current->next;
+// } // Reach the node before the node to be deleted
+// ListNode* nodeToDelete = current->next; // Prevent memory leak?
+// current->next = current->next->next;
+// delete nodeToDelete;
+// ListNode* newList = dummy->next;
+// delete dummy;
+// return newList;
