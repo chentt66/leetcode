691fcced37dde4cf8ba5a6a52f58c7e5ad870aad
chentt66 <tc3216@columbia.edu>
2025-10-25 14:08:46 -0700

    O(log n) solution

--- find_first_and_last_position_of_element_in_sorted_array.34.cpp
+++ find_first_and_last_position_of_element_in_sorted_array.34.cpp
@@ -1,7 +1,42 @@
 // O(log n) time complexity
-// fill later...
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int firstOccurrence = this->findBound(nums, target, true);
+        if (firstOccurrence == -1) {
+            return vector<int>{-1, -1};
+        }
+        int lastOccurrence = this->findBound(nums, target, false);
+        return vector<int>{firstOccurrence, lastOccurrence};
+    }
 
-
+private:
+    int findBound(vector<int>& nums, int target, bool searchFirst) {
+        int N = nums.size();
+        int begin = 0, end = N - 1;
+        while (begin <= end) {
+            int mid = (begin + end) / 2;
+            if (nums[mid] == target) {
+                if (searchFirst) {
+                    if (mid == begin || nums[mid - 1] != target) {
+                        return mid;
+                    }
+                    end = mid - 1;
+                } else {
+                    if (mid == end || nums[mid + 1] != target) {
+                        return mid;
+                    }
+                    begin = mid + 1;
+                }
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                begin = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
 
 // O(n) time complexity
 class Solution {
