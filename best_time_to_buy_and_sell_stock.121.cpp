168879e90211ce96e67efe9aa0b76f37d7459eab
chentt66 <tc3216@columbia.edu>
2025-10-30 18:57:46 -0700

    Find best time to trade stock

---
+++ best_time_to_buy_and_sell_stock.121.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max_profit = 0;
+        int current_min = prices[0];
+        for (int i = 1; i < prices.size(); ++i) {
+            max_profit = max(max_profit, prices[i] - current_min);
+            current_min = min(current_min, prices[i]);
+        }
+    return max_profit;
+    }
+};
