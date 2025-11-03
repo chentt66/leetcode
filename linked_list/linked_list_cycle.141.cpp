1592349261f77c1bb7b296527a94e41febbca30f
chentt66 <tc3216@columbia.edu>
2025-11-03 13:05:54 -0500

    review fast = head->next

--- linked_list/linked_list_cycle.141.cpp
+++ linked_list/linked_list_cycle.141.cpp
@@ -7,18 +7,21 @@
  * };
  */
 
-// Floyd's Cycle Detection Algorithm: O(n) time, O(1) space
+// Floyd's Cycle Detection Algorithm
+// O(n) time, O(1) space
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
         if (!head) return false;
-        ListNode* fast = head->next;
+        ListNode* fast = head->next; // must be head->next!
+        // e.g. single node [1]. slow is pointed to head, fast is nullptr, no cycle
         ListNode* slow = head;
         while (fast != slow) {
         if (fast == nullptr || fast->next == nullptr) return false;
         // e.g. ... → 1 → 2 → 3 → null.  no cycle
-        // !fast: suppose fast at 2. fast->next->next is null.
-        // !fast->next: suppose fast at 3. fast->next is null. fast->next->next will crash.
+        // check conditions:
+        // !fast: suppose fast pointer at 2. fast->next->next is null.
+        // !fast->next: suppose fast pointer at 3. fast->next is null. fast->next->next will crash.
         fast = fast->next->next;
         slow = slow->next;
         }
@@ -26,7 +29,8 @@ public:
     }
 };
 
-// Track occurences: O(n) time, O(n) space
+// Track occurences
+// O(n) time, O(n) space
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
