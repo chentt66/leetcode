6b8bb666b4f42dff69e62f8944e08b902aad9088
chentt66 <tc3216@columbia.edu>
2025-12-10 21:02:55 -0800

    Check whether two trees are identical

--- binary_tree/same_tree.100.cpp
+++ binary_tree/same_tree.100.cpp
@@ -18,6 +18,8 @@ public:
         if (!p || !q) return false;
         if (p->val != q->val) return false;
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        // or simply:
+        // return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
 
