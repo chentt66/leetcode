bb5d185a1af5f95e1cd5c977e8aaf411fbb7f151
chentt66 <tc3216@columbia.edu>
2025-12-17 21:29:02 -0800

    Python solution for adding numbers in two linked lists

---
+++ python-solutions/add_two_numbers.2.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy_node = ListNode(0)
+        curr = dummy_node # python is always referencing
+        carry = 0
+        while l1 or l2 or carry != 0:
+            val1 = 0 if not l1 else l1.val
+            val2 = 0 if not l2 else l2.val
+            val_sum = val1 + val2 + carry
+            digit = val_sum % 10
+            carry = val_sum // 10 # 0 or 1
+            curr.next = ListNode(val = digit)
+            curr = curr.next
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+        return dummy_node.next
