54bec6d05c6bc1fc6f0c75c80d69d70b980c923a
chentt66 <tc3216@columbia.edu>
2025-12-25 12:10:17 -0800

    Revisit two sum(hashing)

--- two_sum.1.cpp
+++ two_sum.1.cpp
@@ -1,12 +1,19 @@
+// Hashing
+// O(n) time
+// O(n) space
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        std::unordered_map<int, int> seen;
+        std::unordered_map<int, int> seen; // nums value, index
         for (int i = 0; i < nums.size(); ++i) {
             int complement = target - nums[i];
-            if (seen.find(complement) != seen.end()) {
-                return std::vector<int> {seen[complement], i};
-            }
+            // hash look up is O(1)
+            std::unordered_map<int, int>::iterator it = seen.find(complement);
+            if (it != seen.end()) return {it->second ,i};
+            // if (seen.find(complement) != seen.end()) {
+                // However, this performs a second lookup (hashing the key again) to retrieve the value
+                // return {seen[complement], i}; 
+            // }
             seen[nums[i]] = i;
         }
         return {};
