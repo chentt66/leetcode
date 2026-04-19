7b284e3e08f0fdbe558acaaa12a6046bf564ba26
chentt66 <tc3216@columbia.edu>
2026-04-19 16:06:39 -0700

    Python defaultdict

---
+++ hashing/group_anagrams.49.py
@@ -0,0 +1,26 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        from collections import defaultdict
+        groups = defaultdict(list)
+        for word in strs:
+            key = ''.join(sorted(word)) # built in sorted for any iterable
+            groups[key].append(word) #[:] ?
+        return list(groups.values())
+    
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = {}
+        for word in strs:
+            key = ''.join(sorted(word))
+            if key not in groups:
+                groups[key] = []
+            groups[key].append(word)
+        return list(groups.values())
+    
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = {}
+        for word in strs:
+            key = ''.join(sorted(word))
+            groups.setdefault(key, []).append(word)
+        return list(groups.values())
